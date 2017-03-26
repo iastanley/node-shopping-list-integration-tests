b7812dcc3c202a774c974b7fc629a613dafe4825
iastanley <iasvks.craigslist@gmail.com>
2017-03-26 10:32:22 -0700

    added test for GET request to recipes route

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -15,5 +15,30 @@ describe('Recipes', function() {
     return closeServer();
   });
 
-  
+  //normal case for get request
+  it('should return list of recipes on GET', function() {
+      //return a promise
+      return chai.request(app)
+        .get('/recipes')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.length.should.be.at.least(1);
+          //checking for expected keys
+          const expectedKeys = ['id', 'name', 'ingredients'];
+          res.body.forEach(recipe => {
+            recipe.should.be.a('object');
+            recipe.should.include.keys(expectedKeys);
+            recipe.ingredients.should.be.a('array');
+          });
+        });//end of then
+  });//end of it
+
+  //normal case for post request
+
+  //normal case for put request
+
+  //normal case for delete request
+
 });

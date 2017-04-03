e1f4e6e7198f67332ed0101b9dc4f3de5fdae42a
iastanley <iasvks.craigslist@gmail.com>
2017-04-03 09:32:12 -0700

    added test for POST request to recipes

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -33,12 +33,31 @@ describe('Recipes', function() {
             recipe.ingredients.should.be.a('array');
           });
         });//end of then
-  });//end of it
+  });//end of GET test
 
   //normal case for post request
+  it('should add an item on POST', function() {
+    const newRecipe = {name: 'fish stew', ingredients: ['fish', 'broth']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(res => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('name', 'ingredients');
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+      });
+  }); //end of POST test
 
   //normal case for put request
+  it('should update item on PUT', function() {
+
+  }); //end of PUT test
 
   //normal case for delete request
+  it('should detele item on DELETE', function() {
 
-});
+  }); //end of DELETE test
+}); //end of describe block
